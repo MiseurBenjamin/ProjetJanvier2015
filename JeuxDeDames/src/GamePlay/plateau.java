577ee807348d013dc7295d8768f37ac9692e3ca7
MiseurBenjamin <miseur_benjamin@live.be>
2014-12-01 00:33:11 +0100

    Create Project

---
+++ JeuxDeDames/src/GamePlay/plateau.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package GamePlay;
+
+/**
+ * @author Benjamin
+ *
+ */
+public class plateau {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
